6b9b8be2f78517749ab25fb2d672fb1806ee8278
KremerMichael <kremeremichael@gmail.com>
2021-04-09 17:00:11 -0400

    auto push run at Fri Apr  9 17:00:11 EDT 2021

--- hw4/p4_21b.m
+++ hw4/p4_21b.m
@@ -3,11 +3,12 @@ time=3; Ts=1/20000;                    % time and sampling interval
 freqs=[0 0.29 0.3 0.4 0.41 1];         % filter design, bandlimited
 amps=[0 0 1 1 0 0];                    % ...between 3K and 4K
 b=firpm(100,freqs,amps);               % BP filter
-f3=2500;
+f3=3500;
+x=filter(b,1,2*randn(1,time/Ts));      % do the filtering
 N=length(x);                         % length of the signal x
 t=Ts*(1:N);                          % define time vector
 n=0.1*cos(2*pi*f3*t); % generate white noise signal
-x=filter(b,1,2*randn(1,time/Ts));      % do the filtering
+
 y=filter(b,1,x+n);                     % (a) filter the signal+noise
 yx=filter(b,1,x);                      % or (b) filter signal 
 yn=filter(b,1,n);                      % ...and noise separately
