7abf2bc9c0ceed5cfc1326c39389af9f175cf2c3
KremerMichael <kremeremichael@gmail.com>
2021-05-05 15:48:18 -0400

    auto push run at Wed May  5 15:48:18 EDT 2021

--- final/AMlarge.m
+++ final/AMlarge.m
@@ -2,7 +2,8 @@
 time=0.33; Ts=1/10000;              % sampling interval & time
 t=0:Ts:time; lent=length(t);        % define a time vector
 fm=20; fc=1000; c=cos(2*pi*fc*t);   % define carrier at freq fc
-w=-10/lent*[1:lent]+cos(2*pi*fm*t);  % create "message" > -1
+w=-10/lent*[1:lent]+cos(2*pi*fm*t); % create "message" > -1
+w
 v=c.*w+c;                           % modulate w/ large carrier
 fbe=[0 0.05 0.1 1];                 % LPF design
 damps=[1 1 0 0]; fl=100; 
